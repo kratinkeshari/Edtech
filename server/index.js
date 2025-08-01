767b46e3195e8954d8198e785c97326566bb7f0d
Kratin Keshari <kratink321@gmail.com>
2025-08-02 00:24:13 +0530

    Resolved cors error

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin: "http://localhost:3000",
+		origin: "https://edtech-dun.vercel.app/",
 		credentials: true,
 	})
 );
